66506dfb737f85226f610cb42642531a521c9a5b
coskunyalcinkaya <coskun@deplike.com>
2019-09-22 09:44:15 +0300

    initial commit.

---
+++ app/src/main/java/com/coskun/simplegithubbrowser/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coskun.simplegithubbrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
